6966a2e1384d00ebf844d0761984976b94395da3
Kamal Kishor <kkishor0297@gmail.com>
2026-01-28 23:50:08 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,3 @@
-
 const CACHE_NAME = 'transworld-ai-cache-v1';
 const urlsToCache = [
   '/',
@@ -9,16 +8,5 @@ const urlsToCache = [
   '/manifest.json'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
+self.addEventListener('install', e=> e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache))));
+self.addEventListener('fetch', e=> e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
